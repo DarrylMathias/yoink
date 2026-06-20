2b1c7f220ce405831cca59d85a12c89109d58bf1
Darryl Mathias <mathiasndarryl7@gmail.com>
2026-06-20 17:23:43 +0530

    changes

--- crawler/validate/hashtable/hash.go
+++ crawler/validate/hashtable/hash.go
@@ -5,11 +5,11 @@ import (
 	"sync/atomic"
 	"yoink/app"
 
-	"yoink/models"
-	"yoink/utils/database"
+	// "yoink/models"
+	// "yoink/utils/database"
 	myredis "yoink/utils/redis"
 	"github.com/redis/go-redis/v9"
-	"gorm.io/gorm"
+	// "gorm.io/gorm"
 )
 
 func AlreadySeen(hashedURL string) (bool, error){
@@ -25,16 +25,17 @@ func AlreadySeen(hashedURL string) (bool, error){
 	if err == redis.Nil {
 		atomic.AddInt64(&app.CacheMiss, 1)
 
-		page := new(models.Page)
-		db := database.DB
-		err := db.Where("url_hash = ?", hashedURL).First(page).Error
+		// db expensive
+		// page := new(models.Page)
+		// db := database.DB
+		// err := db.Where("url_hash = ?", hashedURL).First(page).Error
 
-		if err != nil {
-			if err == gorm.ErrRecordNotFound {
-				return false, nil
-			}
-			return false, err
-		}
+		// if err != nil {
+		// 	if err == gorm.ErrRecordNotFound {
+		// 		return false, nil
+		// 	}
+		// 	return false, err
+		// }
 		if err := myredis.SetCache(hashedURL, "1"); err != nil {
 			fmt.Println("redis set failed:", err)
 		}
