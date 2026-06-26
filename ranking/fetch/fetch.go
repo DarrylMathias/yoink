b365c52bea2b196db54176d3d85952638db9511a
Darryl Mathias <mathiasndarryl7@gmail.com>
2026-06-26 23:13:56 +0530

    even more logs to breakup time

--- ranking/fetch/fetch.go
+++ ranking/fetch/fetch.go
@@ -1,6 +1,8 @@
 package fetch
 
 import (
+	"fmt"
+	"time"
 	"yoink/ranking/file"
 	"yoink/ranking/load"
 
@@ -11,22 +13,31 @@ func FetchAllDocs(token string) (*map[uuid.UUID]int32, error){
 	// doc := make(map[uuid.UUID]float64)
 
 	// get lexicon filenames
+	t1 := time.Now().UnixMilli()
 	lexiconFiles, err := file.GetFiles(".json")
 	if err != nil{
 		return nil, err
 	}
+	t2 := time.Now().UnixMilli()
+	fmt.Printf("get lexicon filenames took %d ms", t2-t1)
 
 	// get lexicon
+	t1 = time.Now().UnixMilli()
 	lexicon, err := load.LoadOffsets(lexiconFiles, token)
 	if err != nil{
 		return nil, err
 	}
+	t2 = time.Now().UnixMilli()
+	fmt.Printf("load offsets took %d ms", t2-t1)
 
 	// get postings map
+	t1 = time.Now().UnixMilli()
 	postings, err := load.LoadPostings(lexicon)
 	if err != nil{
 		return nil, err
 	}
+	t2 = time.Now().UnixMilli()
+	fmt.Printf("load postings took %d ms", t2-t1)
 
 	return postings, nil
 }
